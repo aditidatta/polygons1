093c27a76ebc9545d8c01b200421e80e3599775e
Aditi Datta <aditidatta@live.com>
2017-10-22 02:36:55 +0000

    finishing css for projects and other articles

--- public/js/controllers/ProjCtrl.js
+++ public/js/controllers/ProjCtrl.js
@@ -8,11 +8,11 @@ angular.module('ProjCtrl', []).controller('ProjectController', function($scope, 
     });*/
 
     $scope.projectlist = [
-        { title: 'Project 1', fname: 'proj1.html' },
-        { title: 'Project 2', fname: 'proj2.html' },
-        { title: 'Project 3', fname: 'proj3.html' },
-        { title: 'Project 4', fname: 'proj4.html' },
-        { title: 'Project 5', fname: 'proj5.html' }
+        { title: 'Project 1 ........', fname: 'proj1.html' },
+        { title: 'Project 2...........', fname: 'proj2.html' },
+        { title: 'Project 3.....', fname: 'proj3.html' },
+        { title: 'Project 4..............', fname: 'proj4.html' },
+        { title: 'Project 5....', fname: 'proj5.html' }
     ];
 
     $scope.filePath = 'views/' + $scope.projectlist[0].fname;
